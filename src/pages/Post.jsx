d1830da4f200d60489a67e5b828bf7a66e3afec7
Yujin Son <y09urt77@gmail.com>
2022-12-24 15:24:55 +0900

    Post.jsx의 리액트작업 주석처리

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,21 +1,7 @@
-
 import React from "react";
 import UserInput from "../redux/components/post/UserInput";
 
 function Post({}) {
-  const [postTitle, setPostTitle] = useState(""); // 유진 -포스트제목
-  const [postContents, setPostContents] = useState(""); // 유진 - 포스트내용
-  const [postNick, setPostNick] = useState(""); // 유진 - 닉네임
-  const [postPW, setPostPW] = useState(""); // 유진 - 포스트pw
-  // 카테고리 추가 필요함!!!
-
-  const newPost = {
-    // 카테고리 추가 필요
-    title: postTitle,
-    contents: postContents,
-    nickname: postNick,
-  };
-
   return (
     <>
       <div>
@@ -27,3 +13,27 @@ function Post({}) {
 
 export default Post;
 
+// function Post({}) {
+//   const [postTitle, setPostTitle] = useState(""); // 유진 -포스트제목
+//   const [postContents, setPostContents] = useState(""); // 유진 - 포스트내용
+//   const [postNick, setPostNick] = useState(""); // 유진 - 닉네임
+//   const [postPW, setPostPW] = useState(""); // 유진 - 포스트pw
+//   // 카테고리 추가 필요함!!!
+
+//   const newPost = {
+//     // 카테고리 추가 필요
+//     title: postTitle,
+//     contents: postContents,
+//     nickname: postNick,
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <UserInput />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Post;
