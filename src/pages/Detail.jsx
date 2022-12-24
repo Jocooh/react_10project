6f28770b240134e872e537546724c597bfc746a2
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-24 15:11:06 +0900

    [fix]오류 해결
    충돌 후 오류난거 해결

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Contents from '../redux/components/Detail/Contents';
+import Contents from "../redux/components/Detail/Contents";
 
 export default function Detail() {
-  const {id} = useParams()
-  return <div>
-    {id}번 게시물 상세페이지
-     <Contents />
-  </div>;
-
-
+  const { id } = useParams();
+  return (
+    <div>
+      {id}번 게시물 상세페이지
+      <Contents />
+    </div>
+  );
+}
--- src/redux/components/Header/Header.jsx
+++ src/redux/components/Header/Header.jsx
@@ -2,40 +2,59 @@ import React from "react";
 
 import { useNavigate } from "react-router-dom";
 
-
 import {
-    StyledHeader,
-    StyledLi,
-    StyledNav,
-    StyledUl,
-    StyledHeaderLogoImg,
+  StyledHeader,
+  StyledLi,
+  StyledNav,
+  StyledUl,
+  StyledHeaderLogoImg,
 } from "./styles";
 
 export default function Header() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-
-    return (
-        <StyledHeader>
-            <StyledHeaderLogoImg src='assets/Logo.png' />
-            <StyledNav>
-                <StyledUl>
-
-                    <StyledLi onClick={()=> {navigate("/");}}>최신글</StyledLi>
-                    <StyledLi onClick={()=> {navigate("/life");}}>인생</StyledLi>
-                    <StyledLi onClick={()=> {navigate("/relationship");}}>인간관계</StyledLi>
-                    <StyledLi onClick={()=> {navigate("/family");}}>가족</StyledLi>
-                    <StyledLi onClick={()=> {navigate("/hobby");}}>취미</StyledLi>
-
-                    <StyledLi>최신글</StyledLi>
-                    <StyledLi>인생</StyledLi>
-                    <StyledLi>인간관계</StyledLi>
-                    <StyledLi>가족</StyledLi>
-                    <StyledLi>취미</StyledLi>
-
-                </StyledUl>
-            </StyledNav>
-        </StyledHeader>
-    );
+  return (
+    <StyledHeader>
+      <StyledHeaderLogoImg src="assets/Logo.png" />
+      <StyledNav>
+        <StyledUl>
+          <StyledLi
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            최신글
+          </StyledLi>
+          <StyledLi
+            onClick={() => {
+              navigate("/life");
+            }}
+          >
+            인생
+          </StyledLi>
+          <StyledLi
+            onClick={() => {
+              navigate("/relationship");
+            }}
+          >
+            인간관계
+          </StyledLi>
+          <StyledLi
+            onClick={() => {
+              navigate("/family");
+            }}
+          >
+            가족
+          </StyledLi>
+          <StyledLi
+            onClick={() => {
+              navigate("/hobby");
+            }}
+          >
+            취미
+          </StyledLi>
+        </StyledUl>
+      </StyledNav>
+    </StyledHeader>
+  );
 }
