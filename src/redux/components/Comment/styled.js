55482b8acca36a4802d45400421129be02224d0d
Yujin Son <y09urt77@gmail.com>
2022-12-25 00:05:35 +0900

    comment리스트 댓글+수정삭제 아이콘 가로정렬로 수정

--- src/redux/components/Comment/styled.js
+++ src/redux/components/Comment/styled.js
@@ -1,61 +1,74 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Comment Css
 export const StyledCommentSection = styled.section`
-    /* width: 820px; */
-    min-height: 400px;
-    padding: 0 20px;
-    margin: 30px;
+  /* width: 820px; */
+  min-height: 400px;
+  padding: 0 20px;
+  margin: 30px;
 `;
 
+// 댓글 입력
 export const StyledCommentForm = styled.form`
-    margin: 0 10px;
-`;
+  margin: 0 10px;
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+// 댓글 입력 input
 export const StyledCommentInput = styled.input`
-    margin: 0 15px 0 0;
-    width: 400px;
-    height: 40px;
-    border-radius: 15px;
-    border: none;
-    padding-left: 10px;
-    background-color: rgba(234, 207, 243, 0.295);
+  /* margin: 0 15px 0 0; */ // 유진 - 간격 맞추려고 주석처리했습니다
+  width: 440px;
+  height: 40px;
+  border-radius: 15px;
+  border: none;
+  padding-left: 10px;
+  background-color: rgba(234, 207, 243, 0.295);
 `;
 
 export const StyledPwtInput = styled.input`
-    width: 170px;
-    height: 40px;
-    margin-left: 10px;
-    border-radius: 15px;
-    border: none;
-    padding-left: 10px;
-    background-color: rgba(234, 207, 243, 0.295);
+  width: 170px;
+  height: 40px;
+  /* margin-left: 10px; */ // 유진 - 간격 맞추려고 주석처리
+  border-radius: 15px;
+  border: none;
+  padding-left: 10px;
+  background-color: rgba(234, 207, 243, 0.295);
 `;
 
 // 지은 : 댓글 추가 버튼
 export const StyledCommentButton = styled.button`
-    width: 70px;
-    height: 40px;
-    float: right;
-    border: none;
-    border-radius: 15px;
-    cursor: pointer;
-    font-size: medium;
-    font-weight: bold;
-    background-color: rgba(234, 207, 243, 0.295);
-    &:hover {
-        background-color: #e7b5f7;
-    }
+  width: 70px;
+  height: 40px;
+  float: right;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  font-size: medium;
+  font-weight: bold;
+  background-color: rgba(234, 207, 243, 0.295);
+  &:hover {
+    background-color: #e7b5f7;
+  }
 `;
-
 
 // Comment List Css
 
 // 댓글 리스트 li 태그
 export const StyledCommentLi = styled.li`
-    /* border: 1px solid black; */
-    background-color: rgba(234, 207, 243, 0.26);
-    margin: 10px;
-    padding: 10px;
-    border-radius: 10px;
+  /* border: 1px solid black; */
+  background-color: rgba(234, 207, 243, 0.26);
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  width: 580px;
+`;
+
+export const StyledCommentLiIcon = styled.div`
+  // 유진 - 아이콘까지 가로로 정렬하려고 코멘트+수정삭제 아이콘이 묶인 div태그를 StyledCommentLiIcon로 바꿨어요!
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
