df84e736de20df3a727af566bb2cbeb99682eea9
Jieun Han <yjyyls66@naver.com>
2022-12-25 18:05:07 +0900

    PostDetail.jsx return문 수정
    PostDetail.jsx return문 수정

--- src/redux/components/Detail/PostDetail.jsx
+++ src/redux/components/Detail/PostDetail.jsx
@@ -9,49 +9,57 @@ import { useParams } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 
 import {
-  StyledSection,
-  StyledIconSpanDiv,
-  StyledPostBoxDiv,
-  StyledSpan,
-  StyledPostP,
+    StyledSection,
+    StyledIconSpanDiv,
+    StyledPostBoxDiv,
+    StyledSpan,
+    StyledPostP,
 } from "./styled";
 
 export default function PostDetail() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [title, setTitle] = useState("하...");
+    const [modalOpen, setModalOpen] = useState(false);
+    const [title, setTitle] = useState("하...");
 
-  const articles = useSelector((state) => state.Article);
+    const articles = useSelector((state) => state.Article);
 
-  const titles = useSelector((state) => state);
-  console.log(titles);
+    const titles = useSelector((state) => state);
+    console.log(titles);
 
-  const modalHandle = () => {
-    setModalOpen(true);
-    document.body.style.overflow = "hidden"; //모달창 오픈 시 배경 스크롤 비활성화
-  };
+    const modalHandle = () => {
+        setModalOpen(true);
+        document.body.style.overflow = "hidden"; //모달창 오픈 시 배경 스크롤 비활성화
+    };
 
-  const params = useParams();
-  console.log("params", params);
+    const params = useParams();
+    console.log("params", params);
 
-  // const please = articles.map((item, i) => {
-  //   return item.id === params ? setTitle(item.title) : "다시해라";
-  // });
+    // const please = articles.map((item, i) => {
+    //   return item.id === params ? setTitle(item.title) : "다시해라";
+    // });
 
-  return (
-    <StyledSection>
-      <blockquote>
-        <h4>{title}</h4>
-        <CgProfile />
-        <span>{articles.userName}</span>
-        <MdOutlineWatchLater />
-        <span>2022.10.22</span>
-        <RiEditBoxLine onClick={modalHandle} style={{ cursor: "pointer" }} />
-        <RiDeleteBinLine onClick={modalHandle} style={{ cursor: "pointer" }} />
-        {modalOpen && <ModalBox setModalOpen={setModalOpen} />}
-        <p>튜터님이 저희 팀원 해주세요....🤣</p>
-      </blockquote>
-    </StyledSection>
-  );
+    return (
+        <StyledSection>
+            <StyledPostBoxDiv>
+                <StyledIconSpanDiv>
+                    <h3>{title}</h3>
+                    <CgProfile />
+                    <StyledSpan>{articles.userName}</StyledSpan>
+                    <MdOutlineWatchLater />
+                    <StyledSpan>22.12.24 13:00</StyledSpan>
+                    <RiEditBoxLine
+                        onClick={modalHandle}
+                        style={{ cursor: "pointer", marginLeft: "30px" }}
+                    />
+                    <RiDeleteBinLine
+                        onClick={modalHandle}
+                        style={{ cursor: "pointer", marginLeft: "15px" }}
+                    />
+                    {modalOpen && <ModalBox setModalOpen={setModalOpen} />}
+                </StyledIconSpanDiv>
+                <StyledPostP>튜터님이 저희 팀원 해주세요....🤣</StyledPostP>
+            </StyledPostBoxDiv>
+        </StyledSection>
+    );
 }
 
 const StyledSection = styled.section`
@@ -62,4 +70,3 @@ const StyledSection = styled.section`
 `;
 
 // 유진등장
-// 보였으면 좋겠다
--- src/redux/components/post/UserInput.jsx
+++ src/redux/components/post/UserInput.jsx
@@ -9,102 +9,107 @@ import { v4 as uuidv4 } from "uuid";
 import { useDispatch, useSelector } from "react-redux";
 import { addTitle } from "../../modules/article";
 
-
 function UserInput() {
-  const [title, setTitle] = useState("");
-  const [userName, setUserName] = useState("");
-  const [category, setCategory] = useState(0); //select[0] = 카테고리를선택하세요
-  const [selected, setSelected] = useState(1);
-  const dispatch = useDispatch();
+    const [title, setTitle] = useState("");
+    const [userName, setUserName] = useState("");
+    const [category, setCategory] = useState(0); //select[0] = 카테고리를선택하세요
+    const [selected, setSelected] = useState(1);
+    const dispatch = useDispatch();
 
-  const [pwd, setPwd] = useState("");
-  const [content, setContent] = useState("");
-  const [mainList, setMainList] = useState([]);
+    const [pwd, setPwd] = useState("");
+    const [content, setContent] = useState("");
+    const [mainList, setMainList] = useState([]);
 
+    const useArticle = useSelector((state) => state.Article);
 
-  const useArticle = useSelector((state) => state.Article);
+    // //user title작성
+    // const handleTitle = (e) => {
+    //     setTitle(e.target.value);
+    // };
+    // //user닉네임작성
+    // const handleUser = (e) => {
+    //     setUserName(e.target.value);
+    // };
 
-  //user title작성
-  const handleTitle = (e) => {
-    setTitle(e.target.value);
-  };
-  //user닉네임작성
-  const handleUser = (e) => {
-    setUserName(e.target.value);
-  };
-
-  //카테고리 넘버 지정 함수
-  const handleSelected = (e) => {
-    setSelected(e.target.value);
-  };
-
-  // 글 저장 함수
-  const submitArticle = (e) => {
-    e.preventDefault();
-
-    const newTitle = {
-      title, //현재 title(state)
-      userName,
-      category: selected,
-      id: uuidv4(),
+    //카테고리 넘버 지정 함수
+    const handleSelected = (e) => {
+        setSelected(e.target.value);
     };
 
-    dispatch(addTitle(newTitle));
-    console.log(newTitle);
-  };
+    // 글 저장 함수
+    const submitArticle = (e) => {
+        e.preventDefault();
 
+        const newTitle = {
+            title, //현재 title(state)
+            userName,
+            category: selected,
+            id: uuidv4(),
+        };
 
-  return (
-    <>
-      <form onSubmit={submitArticle}>
-        <InputBody>
-          <InputBox>
-            <p>Selected:{selected}</p>
-            <Select onChange={handleSelected} value={selected}></Select>
-            {/* title */}
-            <Input2
-              id="title"
-              placeholder="제목을 입력해주세요"
-              width="400px"
-              borderRadius="10px"
-              onChange={(e) => {
-                setTitle(e.target.value)}}
-            />
-            {/*User & password  */}
-            <UserPassword>
-              <Input2
-                id="username"
-                placeholder="닉네임"
-                onChange={(e) => {
-                  setUserName(e.target.value)}}
-              />
-              <Input2
-                id="password"
-                placeholder="비밀번호를 입력해주세요"
-                type="password"
-                onChange={(e) => {
-                  setPwd(e.target.value);
-                }}
-              />
-            </UserPassword>
-          </InputBox>
-          {/* 글작성 공간 */}
-          <TextArea type="textarea"  id="content" onChange={(e) => {
-    setContent(e.target.value);
-  }} ></TextArea>
-          {/* <UserInputTextarea id="content" onChange={(e) => {
+        dispatch(addTitle(newTitle));
+        console.log(newTitle);
+    };
+
+    return (
+        <>
+            <form onSubmit={submitArticle}>
+                <InputBody>
+                    <InputBox>
+                        <p>Selected:{selected}</p>
+                        <Select
+                            onChange={handleSelected}
+                            value={selected}
+                        ></Select>
+                        {/* title */}
+                        <Input2
+                            id='title'
+                            placeholder='제목을 입력해주세요'
+                            width='400px'
+                            borderRadius='10px'
+                            onChange={(e) => {
+                                setTitle(e.target.value);
+                            }}
+                        />
+                        {/*User & password  */}
+                        <UserPassword>
+                            <Input2
+                                id='username'
+                                placeholder='닉네임'
+                                onChange={(e) => {
+                                    setUserName(e.target.value);
+                                }}
+                            />
+                            <Input2
+                                id='password'
+                                placeholder='비밀번호를 입력해주세요'
+                                type='password'
+                                onChange={(e) => {
+                                    setPwd(e.target.value);
+                                }}
+                            />
+                        </UserPassword>
+                    </InputBox>
+                    {/* 글작성 공간 */}
+                    <TextArea
+                        type='textarea'
+                        id='content'
+                        onChange={(e) => {
+                            setContent(e.target.value);
+                        }}
+                    ></TextArea>
+                    {/* <UserInputTextarea id="content" onChange={(e) => {
     setContent(e.target.value);
   }} /> */}
-          <div>
-
-            <Button type="submit" style={{ float: "right" }}>
-              저장
-
-          </div>
-        </InputBody>
-      </form>
-    </>
-  );
+                    <div>
+                        <Button type='submit' style={{ float: "right" }}>
+                            저장
+                        </Button>
+                    </div>
+                </InputBody>
+            </form>
+        </>
+    );
 }
 
 export default UserInput;
