1ee6956b990d7845069ec3f40216018dca0c6a77
Yujin Son <y09urt77@gmail.com>
2022-12-25 14:58:23 +0900

    중간점검

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -12,28 +12,3 @@ function Post({}) {
 }
 
 export default Post;
-
-// function Post({}) {
-//   const [postTitle, setPostTitle] = useState(""); // 유진 -포스트제목
-//   const [postContents, setPostContents] = useState(""); // 유진 - 포스트내용
-//   const [postNick, setPostNick] = useState(""); // 유진 - 닉네임
-//   const [postPW, setPostPW] = useState(""); // 유진 - 포스트pw
-//   // 카테고리 추가 필요함!!!
-
-//   const newPost = {
-//     // 카테고리 추가 필요
-//     title: postTitle,
-//     contents: postContents,
-//     nickname: postNick,
-//   };
-
-//   return (
-//     <>
-//       <div>
-//         <UserInput />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Post;
