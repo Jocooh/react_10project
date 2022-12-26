d95afc8f1fc4089d5796b0b86bcfd18abdb42ac7
Jieun Han <yjyyls66@naver.com>
2022-12-26 12:18:02 +0900

    댓글 추가 json server 이용
    댓글 추가 json server 이용 테스트중

--- src/redux/modules/comments.js
+++ src/redux/modules/comments.js
@@ -1,42 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+
 const ADD_COMMENT = "ADD_COMMENT";
-// const REMOVE_COMMENT = "REMOVE_COMMENT";
+const REMOVE_COMMENT = "REMOVE_COMMENT";
 
 export const addComment = (payload) => {
-  return {
-    type: ADD_COMMENT,
-    payload,
-  };
+    return {
+        type: ADD_COMMENT,
+        payload,
+    };
 };
 
-// export const removeComment = (payload) => {
-//     return {
-//         type: REMOVE_COMMENT,
-//         payload,
-//     };
-// };
+export const removeComment = (payload) => {
+    return {
+        type: REMOVE_COMMENT,
+        payload,
+    };
+};
 
 const initialState = [
-  {
-    comment: "블라블라블라",
-  },
-  {
-    comment: "시간 어떻게 불러오지",
-  },
-  {
-    comment: "리덕스 진짜 너무 어렵다",
-  },
-  {
-    comment: "그냥 코딩이 다 어렵다",
-  },
+    // {
+    //     id: uuidv4(),
+    //     comment: "블라블라블라",
+    //     password: 1234,
+    // },
+    // {
+    //     id: uuidv4(),
+    //     comment: "시간 어떻게 불러오지",
+    //     password: 2345,
+    // },
+    // {
+    //     id: uuidv4(),
+    //     comment: "리덕스 진짜 너무 어렵다",
+    //     password: 3456,
+    // },
+    // {
+    //     id: uuidv4(),
+    //     comment: "그냥 코딩이 다 어렵다",
+    //     password: 789,
+    // },
 ];
 
 const comments = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_COMMENT:
-      return [...state, action.payload];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case ADD_COMMENT:
+            return [...state, action.payload];
+        case REMOVE_COMMENT:
+            return state.map((item) => {
+                if (item.id === action.payload) {
+                    return state.filter(
+                        (item) => item.password !== action.payload
+                    );
+                }
+            });
+        default:
+            return state;
+    }
 };
 
 export default comments;
