04731d3f43e90b4c230dcd2c60d903dabd15b74e
rhsok823 <rhsok823@gmail.com>
2022-12-27 18:04:32 +0900

    merge
    11dsfasdf

--- src/redux/components/Detail/PostDetail.jsx
+++ src/redux/components/Detail/PostDetail.jsx
@@ -7,11 +7,9 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 // import { useParams } from "react-router-dom";
 // import Contents from "../redux/components/Detail/Contents";
-
-
 
 
 import {
@@ -25,25 +23,33 @@ import {
 export default function PostDetail() {
   // 비밀번호 모달
   const [modalOpen, setModalOpen] = useState(false);
-  const [title, setTitle] = useState(""); // 유진 - 쓴 글에서 가져와야함
-  const [contents, setContents] = useState(""); // 유진 - 쓴글에서
+  const [title, setTitle] = useState(null); 
+  const [contents, setContents] = useState("");
 
-  // const fetchTitle = async () => {
-  //   const { data } = await axios.get("http://localhost:3001/posts");
-  //   setTitle(data);
-  // }; // 유진
-  // const fetchContents = async () => {
-  //   const { data } = await axios.get("http://localhost:3001/posts");
-  //   setContents(data);
-  // }; // 유진
-  // useEffect(() => {
-  //   fetchTitle();
-  //   fetchContents();
-  // }, []);
+  const paramId = useParams();
+ 
 
-  const articles = useSelector((state) => state.Article);
+  const fetchtitle = async() => {
+    const{ data }= await axios.get("http://localhost:3001/posts");
+    setTitle(data);
+  }
 
-  const titles = useSelector((state) => state);
+  useEffect(() => {
+    fetchtitle();
+    console.log(title);
+  }, []);
+
+  console.log(title)
+
+  
+
+  const selectedDetail = title?.filter((item) =>  item.id == paramId.id)[0];
+
+  console.log(selectedDetail);
+
+  // const articles = useSelector((state) => state.Article);
+
+  // const titles = useSelector((state) => state);
   // console.log(titles);
 
   const modalHandle = () => {
@@ -51,7 +57,8 @@ export default function PostDetail() {
     document.body.style.overflow = "hidden"; //모달창 오픈 시 배경 스크롤 비활성화
   };
 
-  const params = useParams();
+  // const params = useParams();
+
   // console.log("params", params);
 
   // const please = articles.map((item, i) => {
@@ -63,19 +70,13 @@ export default function PostDetail() {
       {/* 지은 : 제목, 내용 적힌 박스를 감싸는 div */}
 
       <StyledPostBoxDiv>
-      <h3>{title}</h3>
-        <h3>{title}</h3>
-        {/* <div>
-          {title?.map((item) => (
-            <p key={item.id}></p>
-          ))}
-        </div> */}
-        {/* 지은 : 작성자, 시간, 수정&삭제 아이콘 부분 감싸는 div */}
+      <h3>{selectedDetail?.title}</h3>
+   
         <StyledIconSpanDiv>
           <CgProfile />
-          <StyledSpan>{articles.userName}</StyledSpan>
+          <StyledSpan>{selectedDetail?.userName}</StyledSpan>
           <MdOutlineWatchLater />
-          <StyledSpan>22.12.24 13:00</StyledSpan>
+          <StyledSpan>{selectedDetail?.date}</StyledSpan>
           <RiEditBoxLine
             onClick={modalHandle}
             style={{ cursor: "pointer", marginLeft: "30px" }}
@@ -87,10 +88,7 @@ export default function PostDetail() {
           {modalOpen && <ModalBox setModalOpen={setModalOpen} />}
         </StyledIconSpanDiv>
         <StyledPostP>
-          {/* {contents?.map((item) => (
-            <p key={item.id}></p>
-          ))} */}
-          튜터님이 저희 팀원 해주세요....🤣
+        {selectedDetail?.contents}
         </StyledPostP>
       </StyledPostBoxDiv>
     </StyledSection>
